fcc297abe8645b64776eef5556c6ff19d5938412
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-06-09 10:56:04 +0700

    a

--- open.sh
+++ open.sh
@@ -1,28 +1,57 @@
 #!/bin/bash
 
 # ======================
-# Config
+# Test Config
 # ======================
 USERS=10
 SPAWN_RATE=1
-RUN_TIME=1m
+RUN_TIME="10s"
+
 HOST="https://hipsbook.gbydigitaltech.co.th"
 
-# ======================
-# Report
-# ======================
-mkdir -p report
+# realistic | stress
+PROFILE="realistic"
 
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+# ======================
+# Report Folder
+# ======================
+DATE=$(date +"%d-%m-%Y")
+TIME=$(date +"%H-%M-%S")
+
+REPORT_DIR="report/${USERS}users_${DATE}_${TIME}"
+
+mkdir -p "$REPORT_DIR"
+
+# ======================
+# Display Config
+# ======================
+echo "==================================="
+echo " Users       : $USERS"
+echo " Spawn Rate  : $SPAWN_RATE"
+echo " Run Time    : $RUN_TIME"
+echo " Profile     : $PROFILE"
+echo " Report Dir  : $REPORT_DIR"
+echo "==================================="
 
 # ======================
 # Run Locust
 # ======================
-py -m locust -f locustfile.py \
+TEST_PROFILE=$PROFILE py -m locust \
+  -f locustfile.py \
   --host="$HOST" \
   --users "$USERS" \
   --spawn-rate "$SPAWN_RATE" \
   --run-time "$RUN_TIME" \
   --headless \
-  --html "report/live_smoke_${TIMESTAMP}.html" \
-  --csv "report/live_smoke_${TIMESTAMP}"
+  --html "$REPORT_DIR/report.html" \
+  --csv "$REPORT_DIR/result"
+
+# ======================
+# Finish
+# ======================
+echo ""
+echo "==================================="
+echo " Test Finished"
+echo " Report saved to:"
+echo " $REPORT_DIR"
+echo "==================================="
