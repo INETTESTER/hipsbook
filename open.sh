17f74c961d1b4b347afe5770c28d5eb0a92eb4d6
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-06-09 11:16:18 +0700

    test

--- open.sh
+++ open.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 # ======================
+# Mode
+# ======================
+# local | master | worker
+MODE="master"
+
+# กรณี Worker
+MASTER_HOST="10.0.0.1"
+
+# กรณี Master
+EXPECT_WORKERS=1
+
+# ======================
 # Test Config
 # ======================
 USERS=10
@@ -26,6 +38,7 @@ mkdir -p "$REPORT_DIR"
 # Display Config
 # ======================
 echo "==================================="
+echo " Mode        : $MODE"
 echo " Users       : $USERS"
 echo " Spawn Rate  : $SPAWN_RATE"
 echo " Run Time    : $RUN_TIME"
@@ -34,24 +47,53 @@ echo " Report Dir  : $REPORT_DIR"
 echo "==================================="
 
 # ======================
-# Run Locust
+# Run
 # ======================
-TEST_PROFILE=$PROFILE py -m locust \
-  -f locustfile.py \
-  --host="$HOST" \
-  --users "$USERS" \
-  --spawn-rate "$SPAWN_RATE" \
-  --run-time "$RUN_TIME" \
-  --headless \
-  --html "$REPORT_DIR/report.html" \
-  --csv "$REPORT_DIR/result"
+
+if [ "$MODE" = "local" ]; then
+
+    TEST_PROFILE=$PROFILE py -m locust \
+      -f locustfile.py \
+      --host="$HOST" \
+      --users "$USERS" \
+      --spawn-rate "$SPAWN_RATE" \
+      --run-time "$RUN_TIME" \
+      --headless \
+      --html "$REPORT_DIR/report.html" \
+      --csv "$REPORT_DIR/result"
+
+elif [ "$MODE" = "master" ]; then
+
+    TEST_PROFILE=$PROFILE py -m locust \
+      -f locustfile.py \
+      --master \
+      --expect-workers "$EXPECT_WORKERS" \
+      --host="$HOST" \
+      --users "$USERS" \
+      --spawn-rate "$SPAWN_RATE" \
+      --run-time "$RUN_TIME" \
+      --headless \
+      --html "$REPORT_DIR/report.html" \
+      --csv "$REPORT_DIR/result"
+
+elif [ "$MODE" = "worker" ]; then
+
+    TEST_PROFILE=$PROFILE py -m locust \
+      -f locustfile.py \
+      --worker \
+      --master-host "$MASTER_HOST"
+
+else
+
+    echo "Invalid MODE: $MODE"
+    exit 1
+
+fi
 
 # ======================
 # Finish
 # ======================
 echo ""
 echo "==================================="
-echo " Test Finished"
-echo " Report saved to:"
-echo " $REPORT_DIR"
+echo " Finished"
 echo "==================================="
